873359ade2725e7ba43338497df30f3be569cf4b
dimicorn <zagorulia.ds@phystech.edu>
2024-12-29 15:21:46 +0300

    red black tree, not working

--- Homeworks/RedBlack/main.c
+++ Homeworks/RedBlack/main.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
-#include "structure.h"
+#include "red_black.h"
 
-int main()
+void inOrder(RBTree* tree, Node* r)
 {
-    printf();
+    Node* nil = tree->nil;
+    Node* root = tree->root;
+
+    if (r == nil || r == NULL)
+        return;
+    inOrder(tree, r->left);
+    printf("%d\n", r->val);
+    inOrder(tree, r->right);
+}
+
+int main(int argc, char** argv)
+{
+    int i, j;
+    const int n = 10;
+    int* arr = (int*)malloc(n * sizeof(int));
+    RBTree* T = (RBTree*)malloc(sizeof(RBTree));
+    Node* nil = createNode(-1, BLACK, NULL, NULL, NULL);
+    Node* root = nil;
+    T->nil = nil;
+    T->root = root;
+
+    for (i = 0; i < n; ++i)
+        arr[i] = rand();
+    for (i = 0; i < n; ++i)
+        printf("%d ", arr[i]);
+    printf("\nFinished inserting\n");
+
+    for (i = 0; i < n; ++i) {
+        insert(T, arr[i]);
+    }
+    printf("%d\n", T->root->val);
+    inOrder(T, T->root);
+    return 0;
 }
---
+++ Homeworks/RedBlack/red_black.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <stdbool.h>
+#include <stdlib.h>
+
+const static bool RED = 0;
+const static bool BLACK = 1;
+
+typedef struct Node
+{
+    int val;
+    bool color;
+    struct Node* left;
+    struct Node* right;
+    struct Node* parent;
+} Node;
+
+Node* createNode(const int v, const bool c, Node* l, Node* r, Node* p);
+
+typedef struct RBTree
+{
+    Node* nil;// = createNode(-1, BLACK, NULL, NULL, NULL);
+    Node* root;// = nil;
+} RBTree;
+
+void leftRotate(RBTree* tree, Node* x);
+void rightRotate(RBTree* tree, Node* x);
+Node* insertBST(RBTree* tree, Node* p, Node* r, const int v);
+void insert(RBTree* tree, const int v);
+Node* findMin(RBTree* tree, Node* r);
+Node* getNode(RBTree* tree, Node* r, const int v);
